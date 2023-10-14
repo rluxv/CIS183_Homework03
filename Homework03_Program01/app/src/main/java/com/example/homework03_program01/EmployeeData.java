ef1d3d37142c9dfe50150e13d814f51fb83acf94
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-14 08:59:30 -0400

    Update EmployeeData.java

--- Homework03_Program01/app/src/main/java/com/example/homework03_program01/EmployeeData.java
+++ Homework03_Program01/app/src/main/java/com/example/homework03_program01/EmployeeData.java
@@ -16,10 +16,21 @@ public class EmployeeData
         return employeeArrayList;
     }
 
-    public boolean usernameAlreadyExists()
+    public boolean usernameAlreadyExists(String username)
     {
-
-
+        //if there are no users in the arraylist, then we would return false
+        if(employeeArrayList.size() == 0)
+        {
+            return false;
+        }
+        for(int i = 0; i < employeeArrayList.size(); i++)
+        {
+            if(employeeArrayList.get(i).getUsername().equalsIgnoreCase(username))
+            {
+                return true;
+            }
+        }
+        //if the function can get thru the loop without returning true, then there are no users in the arraylist that have the same username
         return false;
     }
 
